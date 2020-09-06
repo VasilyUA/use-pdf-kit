8438a89a4704d911d791ee4a5410b1b3cbd326f1
El-dinero <mironenkovasila@gmail.com>
2020-09-06 03:50:15 +0300

    finish

--- createInvoice.js
+++ createInvoice.js
@@ -6,158 +6,92 @@ function createInvoice(invoice, path) {
 
   generateHeader(doc, invoice);
   generateCustomerInformation(doc, invoice);
-  // generateInvoiceTable(doc, invoice);
-  // generateFooter(doc);
+  generateInvoiceTable(doc, invoice);
 
   doc.end();
   doc.pipe(fs.createWriteStream(path));
 }
 
 function generateHeader(doc, invoice) {
+  const marginTop = 55;
+  const date = new Date();
+  const year = date.getFullYear();
+  const getMonth = date.getMonth();
+  const mouth = new Intl.DateTimeFormat("en-US", { month: "long" }).format(
+    getMonth
+  );
+  const getDate = date.getDate();
+
   doc
-    .image("MENS.png", 50, 45, { width: 200, height: 50 })
+    .image("MENS.png", 50, marginTop, { width: 200, height: 50 })
     .fillColor("#444444")
     .fontSize(20)
-    .text("Invoice #:", 110, 50, { align: "right" })
-    .fontSize(20)
-    .text(invoice.invoiceId, 200, 80, { align: "right" })
-    .fontSize(20)
-    .text(`Created:`, 360, 110)
-    .text("new Date", 443, 110)
+    .text("Invoice #:", 110, marginTop, { align: "right" })
+    .text(invoice.invoiceId, 200, marginTop + 24, { align: "right" })
+    .text(`Created: ${year} ${mouth} ${getDate}`, 240, marginTop + 50, {
+      align: "right",
+    })
     .moveDown();
 }
 
 function generateCustomerInformation(doc, invoice) {
+  const marginTop = 180;
+  const { firstName, lastName, email } = invoice.patient.user;
   doc
     .fontSize(20)
-    .text("Mens company", 50, 250, { align: "left" })
-    .text("Street", 50, 270, { align: "left" })
-    .text("Street", 50, 290, { align: "left" })
-    .text("Patient", 50, 250, { align: "right" })
-    .text("Bob Taylor", 50, 270, { align: "right" })
-    .text("pm+1@rivo.agency", 50, 290, { align: "right" })
+    .text("Mens company", 50, marginTop, { align: "left" })
+    .text("Street", 50, marginTop + 20, { align: "left" })
+    .text("Street", 50, marginTop + 40, { align: "left" })
+    .text("Patient", 50, marginTop, { align: "right" })
+    .text(`${firstName} ${lastName}`, 50, marginTop + 20, { align: "right" })
+    .text(email, 50, marginTop + 40, { align: "right" })
     .moveDown();
 }
 
 function generateInvoiceTable(doc, invoice) {
-
-  const invoiceTableTop = 330;
+  let i;
+  const invoiceTableTop = 300;
 
   doc.font("Helvetica-Bold");
-  generateTableRow(
-    doc,
-    invoiceTableTop,
-    "Item",
-    "Description",
-    "Unit Cost",
-    "Quantity",
-    "Line Total"
-  );
-  generateHr(doc, invoiceTableTop + 20);
+  generateTableHeader(doc, invoiceTableTop, "Tupe", "Name", "price");
+  generateHr(doc, invoiceTableTop + 35);
   doc.font("Helvetica");
 
-  // for (let i = 0; i < invoice.items.length; i++) {
-  //   const item = invoice.items[i];
-  //   const position = invoiceTableTop + (i + 1) * 30;
-  //   generateTableRow(
-  //     doc,
-  //     position,
-  //     item.item,
-  //     item.description,
-  //     formatCurrency(item.amount / item.quantity),
-  //     item.quantity,
-  //     formatCurrency(item.amount)
-  //   );
-
-    generateHr(doc, position + 20);
+  for (i = 0; i < invoice.calculationData.length; i++) {
+    const calculationData = invoice.calculationData[i];
+    const { type, name, sum } = calculationData;
+    const position = invoiceTableTop + (i + 1) * 75;
+    generateTableRow(doc, position, type, name, sum);
+    generateHr(doc, position + 45);
   }
+  const totalPosition = invoiceTableTop + (i + 1) * 95;
 
-//   const subtotalPosition = invoiceTableTop + (i + 1) * 30;
-//   generateTableRow(
-//     doc,
-//     subtotalPosition,
-//     "",
-//     "",
-//     "Subtotal",
-//     "",
-//     formatCurrency(invoice.subtotal)
-//   );
+  generateFooter(doc, totalPosition, invoice);
+}
 
-//   const paidToDatePosition = subtotalPosition + 20;
-//   generateTableRow(
-//     doc,
-//     paidToDatePosition,
-//     "",
-//     "",
-//     "Paid To Date",
-//     "",
-//     formatCurrency(invoice.paid)
-//   );
+function generateTableHeader(doc, y, item, unitCost, lineTotal) {
+  doc
+    .fontSize(25)
+    .text(item, 60, y)
+    .text(unitCost, 320, y, { width: 90, align: "right" })
+    .text(lineTotal, 0, y, { align: "right" });
+}
 
-//   const duePosition = paidToDatePosition + 25;
-//   doc.font("Helvetica-Bold");
-//   generateTableRow(
-//     doc,
-//     duePosition,
-//     "",
-//     "",
-//     "Balance Due",
-//     "",
-//     formatCurrency(invoice.subtotal - invoice.paid)
-//   );
-//   doc.font("Helvetica");
-// }
+function generateTableRow(doc, y, item, unitCost, lineTotal) {
+  doc
+    .fontSize(17)
+    .text(item, 50, y - 20)
+    .text(unitCost, 80, y - 20, { width: 350, align: "right" })
+    .text(lineTotal, 0, y - 20, { align: "right" });
+}
 
-// function generateFooter(doc) {
-//   doc
-//     .fontSize(10)
-//     .text(
-//       "Payment is due within 15 days. Thank you for your business.",
-//       50,
-//       780,
-//       { align: "center", width: 500 }
-//     );
-// }
+function generateHr(doc, y) {
+  doc.strokeColor("#aaaaaa").lineWidth(1).moveTo(50, y).lineTo(550, y).stroke();
+}
 
-// function generateTableRow(
-//   doc,
-//   y,
-//   item,
-//   description,
-//   unitCost,
-//   quantity,
-//   lineTotal
-// ) {
-//   doc
-//     .fontSize(10)
-//     .text(item, 50, y)
-//     .text(description, 150, y)
-//     .text(unitCost, 280, y, { width: 90, align: "right" })
-//     .text(quantity, 370, y, { width: 90, align: "right" })
-//     .text(lineTotal, 0, y, { align: "right" });
-// }
-
-// function generateHr(doc, y) {
-//   doc
-//     .strokeColor("#aaaaaa")
-//     .lineWidth(1)
-//     .moveTo(50, y)
-//     .lineTo(550, y)
-//     .stroke();
-// }
-
-// function formatCurrency(cents) {
-//   return "$" + (cents / 100).toFixed(2);
-// }
-
-// function formatDate(date) {
-//   const day = date.getDate();
-//   const month = date.getMonth() + 1;
-//   const year = date.getFullYear();
-
-//   return year + "/" + month + "/" + day;
-// }
-
+function generateFooter(doc, totalPosition, invoice) {
+  doc.fontSize(30).text(`Total: $${invoice.total}`, 70, totalPosition);
+}
 module.exports = {
   createInvoice,
 };
